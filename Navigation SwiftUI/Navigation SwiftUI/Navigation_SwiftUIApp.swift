3c0b274b0da74f90007abf0415783f9513a8fec9
FaNtast2912 <z_maks@inbox.ru>
2025-02-16 10:36:09 +0300

    initial commit

---
+++ Navigation SwiftUI/Navigation SwiftUI/Navigation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_SwiftUIApp.swift
+//  Navigation SwiftUI
+//
+//  Created by Maksim Zakharov on 16.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
